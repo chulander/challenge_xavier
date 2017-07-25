93f1f5dbfb4865627803a6dc1f8c42b1b5358cbf
Bryan Chu <bryan@Bryans-MacBook-Pro.local>
2017-07-25 10:17:21 -0400

    removed _id from stats for avgPremium

--- shell_stats.js
+++ shell_stats.js
@@ -1,4 +1,5 @@
 use xavier
+
 db.getCollection('quotes').find({}).length() + ' total quotes attempted'
 db.getCollection('quotes').find({eligible:true}).length() + ' quotes status = "success" '
 db.getCollection('quotes').find({eligible:false}).length() + ' quotes status = "failure" '
@@ -6,8 +7,17 @@ db.getCollection('quotes').aggregate([
   {$match:{eligible:true}},
   {$group:
     {
-      _id:'placeholder',
+      _id:null,
       avgPremium:{$avg:'$annual_premium'}
     }
+  },
+  {
+    $project:{
+      _id:0,
+      avgPremium:1
+
+    }
+
   }
 ])
+
